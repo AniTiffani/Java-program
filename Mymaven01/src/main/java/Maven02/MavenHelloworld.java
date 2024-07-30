9958f2f5347ae201938e39d568e3750c15954c87
ANI TIFFANI <ANI TIFFANI@LAPTOP-3VTI97PU>
2024-07-30 20:36:20 +0530

    helloworld

---
+++ Mymaven01/src/main/java/Maven02/MavenHelloworld.java
@@ -0,0 +1,14 @@
+package Maven02;
+
+public class MavenHelloworld {
+  public void display()
+  {
+	  System.out.println("hello world");
+  }
+	public static void main(String[] args) {
+		MavenHelloworld obj=new MavenHelloworld();
+		obj.display();
+
+	}
+
+}
